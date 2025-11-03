61ad390380b36bfc56d861f978eeda5ae507ab89
elfaradio <farhadulislam84054@gmail.com>
2025-11-03 13:27:15 +0600

    hlw

---
+++ Lab03/a.cpp
@@ -0,0 +1,23 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define str string
+void solve()
+{
+    // By Farhadul Islam
+    // Only Brute Force Bhai
+}
+int32_t main()
+{
+    int t = 1;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
